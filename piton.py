49b942b827a8641fd004d0873b4c07a9acfc072d
kirill <sn020068@gmailc.om>
2023-12-29 16:37:16 +0300

    домашка

---
+++ piton.py
@@ -0,0 +1,12 @@
+def hello_who(name):
+    return f'Hello,{name}'
+
+def salary(hours,salary_by_hour):
+    k = 2*1
+    return hours * salary_by_hour* k
+
+def privet_who(name):
+    return f'privet,{name}'
+
+def people_who(name):
+    return f'People,{name}'
